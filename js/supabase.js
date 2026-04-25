9365fa7ea75d57d3330c107add7a12c219b7e2f5
Cnobiorah <156436795+Cnobiorah@users.noreply.github.com>
2026-04-25 15:06:52 +0100

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_URL";
-const SUPABASE_KEY = "YOUR_KEY";
+const SUPABASE_URL = "https://snokfmygrgittxvwnumv.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNub2tmbXlncmdpdHR4dndudW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMjUwMTIsImV4cCI6MjA5MjYwMTAxMn0.iCE3WVio6qGD1G6PT6hPOFFcAS0D5J3kQPB7feky92Q";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
